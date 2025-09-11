dd22441f5a5a800d30e83a2ea0fcff12ecfd8513
peterwade153 <peterwade153@gmail.com>
2025-09-11 09:44:57 +0300

    use file processor module

--- app/management/commands/import.py
+++ app/management/commands/import.py
@@ -1,28 +1,21 @@
-import csv
 import hashlib
-import json
 import time
-from enum import Enum
 from typing import Any
 from typing import Optional
 from typing import List
-from typing import Iterator
 from pathlib import Path
 
 from django.contrib.gis.geos import Point
 from django.core.management.base import BaseCommand
 from django.core.management.base import CommandError
 from django.core.management.base import CommandParser
-import lxml.etree as etree
 
 from app.models import PointOfInterest
 from app.models import FileHash
-
-
-class FileFormatEnum(Enum):
-    CSV = ".csv"
-    JSON = ".json"
-    XML = ".xml"
+from app.file_processor.file_formats import FileFormatEnum
+from app.file_processor.processors import CSVFileProcessor
+from app.file_processor.processors import JSONFileProcessor
+from app.file_processor.processors import XMLFileProcessor
 
 
 class Command(BaseCommand):
@@ -39,9 +32,13 @@ class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any) -> Optional[str]:
         chunk_size = 8192
         paths = options["paths"]
+        file_processor_map = {
+            FileFormatEnum.CSV.value : CSVFileProcessor(),
+            FileFormatEnum.JSON.value: JSONFileProcessor(),
+            FileFormatEnum.XML.value: XMLFileProcessor()
+        }
 
         start_time = time.perf_counter()
-
         file_paths = self._get_file_paths(paths=paths)
         if not file_paths:
             raise CommandError("No file paths found")
@@ -58,7 +55,10 @@ class Command(BaseCommand):
                 continue
 
             self._process_file(
-                file_path=file_path, file_hash=file_hash, batch_size=chunk_size
+                file_path=file_path,
+                file_hash=file_hash, 
+                batch_size=chunk_size,
+                file_processor_map = file_processor_map
             )
 
         end_time = time.perf_counter()
@@ -109,36 +109,8 @@ class Command(BaseCommand):
                     break
                 hasher.update(chunk)
         return hasher.hexdigest()
-
-    @staticmethod
-    def _get_rows_from_file(file_path: Path) -> Iterator[dict]:
-        """
-        Reads a file and yields POI data
-        """
-        suffix = file_path.suffix.lower()
-
-        if suffix == FileFormatEnum.CSV.value:
-            with file_path.open("r", encoding="utf-8") as csv_file:
-                reader = csv.DictReader(csv_file)
-                yield from reader
-        elif suffix == FileFormatEnum.JSON.value:
-            with file_path.open("r", encoding="utf-8") as json_file:
-                data = json.load(json_file)
-                if isinstance(data, dict):
-                    yield data
-                else:
-                    yield from data
-        elif suffix == FileFormatEnum.XML.value:
-            for _, elem in etree.iterparse(file_path, events=["end"], recover=True):
-                if elem.tag == "DATA_RECORD":
-                    row = {
-                        child.tag: child.text.strip() if child.text else None
-                        for child in elem
-                    }
-                    yield row
-                    elem.clear()
     
-    def _process_file(self, file_path: Path, batch_size: int, file_hash: str) -> None:
+    def _process_file(self, file_path: Path, batch_size: int, file_hash: str, file_processor_map: dict) -> None:
         """
         Processes a file in batches and inserts records in the database table
         """
@@ -146,9 +118,21 @@ class Command(BaseCommand):
         total_imported = 0
 
         try:
-            row_iter = self._get_rows_from_file(file_path=file_path)
+            processor_key = file_path.suffix.lower()
+            file_processor = file_processor_map.get(processor_key)
+            row_iter = file_processor.read_file_content(file_path=file_path)
             for row in row_iter:
-                batch.append(self._row_to_poi(row=row))
+                row_dict = file_processor.row_to_dict(row=row)
+                poi = PointOfInterest(
+                    external_id=row_dict.get("external_id"),
+                    name=row_dict.get("name"),
+                    description=row_dict.get("description", ""),
+                    category=row_dict.get("category"),
+                    point=Point(float(row_dict.get("latitude")), float(row_dict.get("longitude"))),
+                    average_rating=row_dict.get("average_rating"),
+                    ratings=row_dict.get("ratings"),
+                )
+                batch.append(poi)
                 if len(batch) >= batch_size:
                     self._bulk_insert(batch=batch)
                     total_imported += len(batch)
@@ -168,49 +152,6 @@ class Command(BaseCommand):
                 FileHash.objects.create(file_hash=file_hash)
         except Exception as e:
             self.stdout.write(self.style.ERROR(f"Error processing {file_path}: {e}"))
-
-    @staticmethod
-    def _row_to_poi(row: dict) -> PointOfInterest:
-        """
-        Transforms row dictionary into PointOfInterest model instance,
-        """
-        external_id = row.get("poi_id") or row.get("id") or row.get("pid")
-        name = row.get("poi_name") or row.get("name") or row.get("pname")
-        latitude = (
-            row.get("poi_latitude")
-            or row.get("coordinates", {}).get("latitude")
-            or row.get("platitude")
-        )
-        longitude = (
-            row.get("poi_longitude")
-            or row.get("coordinates", {}).get("longitude")
-            or row.get("plongitude")
-        )
-        category = (
-            row.get("poi_category") or row.get("category") or row.get("pcategory")
-        )
-        description = row.get("description", "")
-
-        ratings = []
-        if "poi_ratings" in row:
-            ratings = row.get("poi_ratings").strip("{}").split(",")
-        elif "ratings" in row:
-            ratings = row.get("ratings")
-        elif "pratings" in row:
-            ratings = row.get("pratings").split(",")
-
-        ratings_values = [float(item) for item in ratings]
-        average_rating = sum(ratings_values) / len(ratings_values)
-
-        return PointOfInterest(
-            external_id=external_id.strip(),
-            name=str(name),
-            description=description.strip(),
-            category=category.strip(),
-            point=Point(float(longitude), float(latitude)),
-            average_rating=average_rating,
-            ratings=ratings_values,
-        )
 
     @staticmethod
     def _bulk_insert(batch: List[PointOfInterest]) -> None:
