c8ba27c6ed750ba2b8e8811e98485befb79c7d68
peterwade153 <peterwade153@gmail.com>
2025-09-11 14:02:36 +0300

    temp file csv file write for test

--- app/tests/test_imports.py
+++ app/tests/test_imports.py
@@ -1,4 +1,5 @@
 import os
+import csv
 from django.test import TestCase
 from django.core.management import call_command
 from app.models import PointOfInterest
@@ -8,13 +9,32 @@ from tempfile import NamedTemporaryFile
 class ImportFileDataCommandTests(TestCase):
 
     def test_csv_file_import(self):
-        csv_file_content = """poi_id,poi_name,poi_category,poi_latitude,poi_longitude,poi_ratings
-    1,ちぬまん,restaurant,26.2155192001422,127.6854314,"{3.0,4.0,3.0,5.0,2.0,3.0,2.0,2.0,2.0,2.0}"
-    2,Otter Creek State Forest,nature-reserve,43.7149419232782,-75.3263056920684,"{3.0,4.0,3.0,5.0,2.0,3.0,2.0,2.0,2.0,2.0}"
-    """
+        data = [
+            {
+                "poi_id": 1, 
+                "poi_name": "ちぬまん", 
+                "poi_category": "restaurant", 
+                "poi_latitude": "26.2155192001422", 
+                "poi_longitude": "127.6854314", 
+                "poi_ratings": "{3.0,4.0,3.0,5.0,2.0,3.0,2.0,2.0,2.0,2.0}"
+            },
+            {
+                "poi_id": 2, 
+                "poi_name": "Otter Creek State Forest", 
+                "poi_category": "nature-reserve", 
+                "poi_latitude": "43.7149419232782", 
+                "poi_longitude": "-75.3263056920684", 
+                "poi_ratings": "{3.0,4.0,3.0,5.0,2.0,3.0,2.0,2.0,2.0,2.0}"
+            }
+        ]
+        fieldnames = ["poi_id", "poi_name", "poi_category", "poi_latitude", "poi_longitude", "poi_ratings"]
         tmp_file_path = ""
         with NamedTemporaryFile(mode="w+", delete=False, suffix=".csv") as tmp_file:
-            tmp_file.write(csv_file_content)
+            writer = csv.DictWriter(tmp_file, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(data)
+
+            # tmp_file.write(csv_file_content)
             tmp_file_path = tmp_file.name
 
         try:
